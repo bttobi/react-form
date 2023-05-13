5701f150afc72583fd148a616548884e9f0a27a9
bttobi <bttobi123@gmail.com>
2023-05-13 15:53:29 +0200

    error types file creation

---
+++ src/data/errorTypes.tsx
@@ -0,0 +1,32 @@
+const errorTypes = [
+  {
+    name: "name",
+    type: "custom",
+  },
+  {
+    name: "preparation_time",
+    type: "focus",
+  },
+  {
+    name: "preparation_time",
+    type: "custom",
+  },
+  {
+    name: "no_of_slices",
+    type: "custom",
+  },
+  {
+    name: "diameter",
+    type: "custom",
+  },
+  {
+    name: "slices_of_bread",
+    type: "custom",
+  },
+  {
+    name: "spiciness_scale",
+    type: "custom",
+  },
+];
+
+export default errorTypes;
