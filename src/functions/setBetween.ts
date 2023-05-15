3fccdcc40b3da2d8f5d66d8c1ec27f83de223d39
bttobi <bttobi123@gmail.com>
2023-05-15 13:52:26 +0200

    Create setBetween.ts

---
+++ src/functions/setBetween.ts
@@ -0,0 +1,26 @@
+const setBetween = (
+  value: string | number,
+  min: string,
+  max: string,
+  setter: React.Dispatch<React.SetStateAction<string>>
+) => {
+  if (
+    value.toString().includes("e") ||
+    value.toString().includes(".") ||
+    value.toString().includes(",") ||
+    value == ""
+  ) {
+    setter(min.toString());
+    return;
+  }
+  if (Number(value) > Number(max)) {
+    setter(max.toString());
+    return;
+  }
+  if (Number(value) < Number(min)) {
+    setter(min.toString());
+    return;
+  } else setter(value.toString());
+};
+
+export default setBetween;
